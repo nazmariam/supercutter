c6a089c7e269440a4d4314732c3510490c8a697b
nazma <nzm@live.ru>
2018-10-24 23:13:36 +0300

    initial commit;

---
+++ js/script.js
@@ -0,0 +1,58 @@
+$( document ).ready(function() {
+
+    var setClock = 600;
+
+    $('.clock').each(function() {
+        var clock = $(this);
+        clock.FlipClock(setClock, {
+            clockFace: 'HourlyCounter',
+            countdown: true,
+            language: 'it',
+            autoStart:true
+        });
+    });
+
+    $('.faq_list h2').click(function (e) {
+        $(this).next('.result').slideToggle();
+        $(this).toggleClass('active');
+    });
+
+    $('select').niceSelect();
+
+    if (screen.width < 768) {
+        $('.slick').slick({
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            dots: true,
+            arrows:false,
+            responsive: [
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        })
+    }
+    function resizeViseo() {
+        var video = $('video');
+        var videoWidth = video.width();
+        var videoHeight = videoWidth*0.60;
+        video.css('height',videoHeight);
+    }
+    resizeViseo();
+
+    $( window ).resize(function() {
+        resizeViseo();
+    });
+
+    $('.play_cover').click(function () {
+        $(this).hide();
+        const vid = document.getElementById("top_video");
+        // vid.controls = false;
+        vid.play();
+    });
+
+});
